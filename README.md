ac87c35295629a41800bf5830d8d3d21ab29775a
Katie Vogel <katiebvogel@gmail.com>
2016-09-14 11:19:51 -0500

    basic app built out includes registration, login, and profile

---
+++ README.md
@@ -0,0 +1,7 @@
+This project was created using a tutorial which familiarizes the learner with several great technologies:
+AngularJS 1.x
+Stormpath
+Yeoman
+Bower
+Grunt
+Node.js
--- client/app/app.js
+++ client/app/app.js
@@ -4,11 +4,20 @@ angular.module('dashboardApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ui.router'
+  'ui.router',
+  'stormpath',
+  'stormpath.templates'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
+  })
+
+  .run(function($stormpath){
+  $stormpath.uiRouter({
+    loginState: 'login',
+    defaultPostLoginState: 'main'
   });
+});
